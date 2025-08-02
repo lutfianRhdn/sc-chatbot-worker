b70014ea68d560be2a4b4c3e9b023185d8fb9e29
Lutfian Rahdiansyah <lutfian.rhdn@gmail.com>
2025-08-02 20:17:16 +0700

    add log

--- src/utils/functions_non_utama.py
+++ src/utils/functions_non_utama.py
@@ -112,6 +112,7 @@ def load_prompt_template(filename):
     # print(f"Loading prompt template from {filename}")
     try:
         base_path = os.path.dirname(os.path.abspath(__file__))
+        print(f"base path:{base_path}")
         json_file_path = os.path.join(base_path, '../prompt/', filename)
         print(f"load prompt : {json_file_path}")
         with open(json_file_path, 'r', encoding='utf-8') as f:
