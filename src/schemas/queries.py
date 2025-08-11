c20f526241d0d90127a4e639e63ce3eee295b45b
LutfianRhdn <lutfian.rhdn@gmail.com>
2025-08-11 18:28:20 +0700

    Update query handling and change prompt retrieval destination to GraphQLWorker

--- src/schemas/queries.py
+++ src/schemas/queries.py
@@ -130,6 +130,7 @@ class Query:
           data=caceData
       )
       if len(result["result"]) == 0:
+          print(projectId)
           result = worker.sendToOtherWorker(
               destination=[f"DatabaseInteractionWorker/getPrompt/{projectId}"],
               data={"key": projectId}
@@ -145,6 +146,8 @@ class Query:
               }
           )
       print(f"Result from worker: {result}")
+      if result['result'] == 'null' or len(result['result']) == 0:
+            return PromptResponse(project_id=projectId, prompt=[])
       result_data = result["result"][0]
       project_id = result_data["project_id"]
       prompts = result_data["prompts"]
